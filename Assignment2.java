cd7b496f773dc64d25afd4e15ef80b6195dc0c67
Lucy <Lucy@Chairman_Vidar>
2011-08-30 09:56:58 -0400

    Added Assignment2

---
+++ Assignment2.java
@@ -0,0 +1,16 @@
+public class Assignment2
+{
+	public static void main (String args[])
+	{
+	int i1=7;
+	int i2=3;
+	int totali= i1/i2;
+	System.out.println("The value of i1 over i2 is:" +totali);
+	double d1=7;
+	double d2=3;
+	double totald=d1/d2;
+	System.out.println("The value of d1 over d2 is:" +totald);
+	double diff=totald-totali;
+	System.out.println("The difference between the values is:" +diff);
+	}
+}
