a33eef26eac3912e8784021c0e0a5eb25db77df7
wilcoxl <wilcoxl@CSPC7.mssm.local>
2011-09-22 14:13:44 -0400

    Added Assignment10

---
+++ InClass921/Item.java
@@ -0,0 +1,17 @@
+public class Item
+{
+	String name;
+	double price;
+
+
+	public void setName(String itemName)
+	{
+		name=itemName;
+	}
+
+	public void setPrice(double itemPrice)
+	{
+		price=itemPrice;
+	}
+	
+}
